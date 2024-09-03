71779ef7262acd34464254e31ccff7d51471328e
Marlina Marlina <marlina.juju@outlook.com>
2024-09-03 14:12:43 +0700

    amplify initialization, create graphql task management

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "taskmanagement": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
